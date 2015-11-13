1d1171bb80a77899c01fed772b54f848c0ab5824
VmVegeta <vemund@vemund-HP-EliteBook-850-G1>
2015-11-13 11:17:56 +0100

    0.0.2 second green test turn String to int

--- ObligTDD/src/TDDTaskCode/Utility.java
+++ ObligTDD/src/TDDTaskCode/Utility.java
@@ -2,9 +2,17 @@ package TDDTaskCode;
 
 public class Utility {
 
-	public Object turnToInt(String valueTo1) {
+	public int turnToInt(String bitString) {
 		
-		return 1;
+		int value = 0;
+		
+		for(int i = 0; i < bitString.length(); i++){
+			if(bitString.charAt(i) == '1'){
+				value = value + (int) Math.pow(2, bitString.length() - i - 1);
+			}
+		}
+		
+		return value;
 	}
 
 }
--- ObligTDD/src/test/UtilityTest.java
+++ ObligTDD/src/test/UtilityTest.java
@@ -11,9 +11,15 @@ public class UtilityTest {
 	Utility utility = new Utility();
 
 	@Test
-	public void test() {
+	public void turnToInt_1() {
 		String valueTo1 = "0000001";
 		assertEquals(1, utility.turnToInt(valueTo1));
+	}
+	
+	@Test
+	public void turnToInt_2() {
+		String valueTo2 = "00000010";
+		assertEquals(2, utility.turnToInt(valueTo2));
 	}
 
 }
