0cbf4ba2fc384addc74671215595520a0e65fb20
VmVegeta <vemund@vemund-HP-EliteBook-850-G1>
2015-11-15 16:10:34 +0100

    0.0.6		24 green tests added new exception and turn to hex methode

--- ObligTDD/src/TDDTaskCode/Utility.java
+++ ObligTDD/src/TDDTaskCode/Utility.java
@@ -6,14 +6,18 @@ public class Utility {
 		if(bitString.length() > 24){
 			throw new IllegalArgumentException();
 		}
-		
+		for(int i = 0; i < bitString.length(); i++){
+			if(bitString.charAt(i) != '0' && bitString.charAt(i) != '1'){
+				throw new IllegalArgumentException();
+			}
+		}
+
 		int value = 0;
 		for(int i = 0; i < bitString.length(); i++){
 			if(bitString.charAt(i) == '1'){
 				value += (int) Math.pow(2, bitString.length() - i - 1);
 			}
 		}
-		
 		return value;
 	}
 
@@ -21,38 +25,73 @@ public class Utility {
 		if(value > 16777215 || value < 0){
 			throw new IllegalArgumentException();
 		}
-		
-		StringBuilder string = new StringBuilder();
-		
+
+		StringBuilder bitString = new StringBuilder();
+
 		for(int i = 23; i >= 0; i--){
 			if(value >= Math.pow(2, i)){
-				string.append('1');
+				bitString.append('1');
 				value -= Math.pow(2, i);
 			}else{
-			string.append('0');
+				bitString.append('0');
 			}
 		}	
-		
-		String bitString = string.toString();
-		return bitString;
+
+		return bitString.toString();
 	}
 
 	public int turnToIntFromHex(String hex) {
 		if(hex.length() > 6){
 			throw new IllegalArgumentException();
 		}
-		
+		for(int i = 0; i < hex.length(); i++){
+			if(hex.charAt(i) != '0' && hex.charAt(i) != '1' && hex.charAt(i) != '2' && hex.charAt(i) != '3' &&
+					hex.charAt(i) != '4' && hex.charAt(i) != '5' && hex.charAt(i) != '6' && hex.charAt(i) != '7' &&
+					hex.charAt(i) != '8' && hex.charAt(i) != '9' && hex.charAt(i) != 'a' && hex.charAt(i) != 'b' &&
+					hex.charAt(i) != 'c' && hex.charAt(i) != 'd' && hex.charAt(i) != 'e' && hex.charAt(i) != 'f'){
+				throw new IllegalArgumentException();
+			}
+		}
+
 		int value = 0;
 		for(int i = 0; i < hex.length(); i++){
 			for(int j = 0; j < 9; j++){
 				if(hex.charAt(i) == '0'+j){
 					value += j * (int) Math.pow(16, hex.length() - i - 1);
 				}
-				else if(hex.charAt(i) == 'A'+j){
+				else if(hex.charAt(i) == 'a'+j){
 					value += (10+j) * (int) Math.pow(16, hex.length() - i - 1);
 				}
 			}
 		}
 		return value;
+	}
+
+	public String turnToHex(int value) {
+		if(value > 16777215 || value < 0){
+			throw new IllegalArgumentException();
+		}
+
+		StringBuilder hexString = new StringBuilder();
+
+		for(int i = 5; i >= 0; i--){
+			if(value >= Math.pow(16, i)){
+				for(int j = 15; j >= 0; j--){
+					if(value >= j*Math.pow(16,i)){
+						if(j >= 10){
+							hexString.append((char)('a'-10+j));
+						}else{
+							hexString.append(j);
+						}
+					value -= Math.pow(2, i);
+					break;
+					}
+				}
+			}else{
+				hexString.append('0');
+			}
+		}
+
+		return hexString.toString();
 	}
 }
--- ObligTDD/src/test/UtilityTest.java
+++ ObligTDD/src/test/UtilityTest.java
@@ -23,9 +23,15 @@ public class UtilityTest {
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void turnToInt_underSize24() {
+	public void turnToInt_overSize24() {
 		String toBig = "00000000000000000000000000";
 		assertEquals(0,utility.turnToInt(toBig));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void turnToInt_unknownSign() {
+		String unknownSigns = "qwer45376+!3#";
+		assertEquals(0,utility.turnToInt(unknownSigns));
 	}
 	
 	@Test
@@ -84,13 +90,13 @@ public class UtilityTest {
 	
 	@Test
 	public void turnToIntFromHex_31() {
-		String valueTo31 = "00001F";
+		String valueTo31 = "00001f";
 		assertEquals(31, utility.turnToIntFromHex(valueTo31));
 	}
 	
 	@Test
 	public void turnToIntFromHex_highEdge() {
-		String valueOfHigh = "FFFFFF";
+		String valueOfHigh = "ffffff";
 		assertEquals(16777215, utility.turnToIntFromHex(valueOfHigh));
 	}
 	
@@ -102,7 +108,49 @@ public class UtilityTest {
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void turnToIntFromHex_over6() {
-		String valueOfOver = "0000000";
-		assertEquals(0, utility.turnToIntFromHex(valueOfOver));
+		String overSixLength = "0000000";
+		assertEquals(0, utility.turnToIntFromHex(overSixLength));
 	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void turnToHex_unknownSigns() {
+		String unknownSigns = "s3G#å!";
+		assertEquals(0, utility.turnToIntFromHex(unknownSigns));
+	}
+	
+	@Test
+	public void turnToHex_1() {
+		String hexOf1 = "000001";
+		assertEquals(hexOf1, utility.turnToHex(1));
+	}
+	
+	@Test
+	public void turnToHex_31() {
+		String hexFor31 = "00001f";
+		assertEquals(hexFor31, utility.turnToHex(31));
+	}
+	
+	@Test
+	public void turnToHex_lowEdge() {
+		String hexForLowEdge = "000000";
+		assertEquals(hexForLowEdge, utility.turnToHex(0));
+	}
+	
+	@Test
+	public void turnToHex_highEdge() {
+		String hexForHighEdge = "ffffff";
+		assertEquals(hexForHighEdge, utility.turnToHex(16777215));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void turnToHex_overEdge() {
+		assertEquals("", utility.turnToHex(16777216));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void turnToHex_underEdge() {
+		assertEquals("", utility.turnToHex(-1));
+	}
+	
+	
 }
